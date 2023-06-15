5080c079e96e9216761cf0737439ba1a962ec752
victory okirie <victoryokirie98@gmail.com>
2023-06-15 14:19:29 -0700

    puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Author: Victory Okirie
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char message[75] = "\"Programming is like building a multilingual puzzle\"";
+
+	puts(message);
+
+	return (0);
+}
