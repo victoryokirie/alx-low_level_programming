aaab013f7bf7e0ea89257080a70a7f173ce47497
victory okirie <victoryokirie98@gmail.com>
2023-06-17 13:21:39 -0700

    positive or negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Author: Victory Okirie
+ *
+ * Date: 17th June 2023
+ *
+ * Details: A c program to check if a number is +ve, -ve or 0
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n == 0)
+		printf("%d is zero\n, n");
+	else if (n > 0)
+		printf("%d is positive\n, n")
+	else if (n < 0)
+		printf("%d is negative\n, n")
+	return (0);
+}
