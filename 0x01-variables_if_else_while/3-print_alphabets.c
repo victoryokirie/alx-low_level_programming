87ce6d14e6bdc28e95bbf456fe5bfdd56be14874
victory okirie <victoryokirie98@gmail.com>
2023-06-18 03:05:20 -0700

    Alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Author: Victory Okirie
+ *
+ * Return: 0(Success)
+ */
+int main(void)
+{
+	char c;
+	char C;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+		putchar(c);
+	for (C = 'A' ; C <= 'Z' ; C++)
+		putchar(C);
+	putchar('\n');
+	return (0);
+}
