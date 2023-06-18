1d8598a015e2965b432990a13581aef95e1751a2
victory okirie <victoryokirie98@gmail.com>
2023-06-18 09:56:10 -0700

    possible combo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0(Success)
+ */
+int main(void)
+{
+	int num1;
+	int num2;
+
+	for (num1 = 48; num1 <=57; num1++)
+	{
+		for (num2 = 49; num2 <= 57; num2++)
+		{
+			if (num1 != num2 && num2 > num1)
+			{
+				putchar(num1);
+				putchar(num2);
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
