fb9aa8bdc2075e462f9c7f6376dc37151c884bcb
victory okirie <victoryokirie98@gmail.com>
2023-06-18 20:41:50 -0700

    3 digit combo

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+/**
+* main - Entry point
+*
+* Return: 0(Success)
+*/
+int main(void)
+{
+	int num1;
+	int num2;
+	int num3;
+
+	for (num1 = 48; num1 <= 57; num1++)
+	{
+		for (num2 = 49; num2 <= 57; num2++)
+		{
+			for (num3 = 50; num3 <= 57; num3++)
+			{
+				if (num1 != num2 && num2 != num3 && num2 > num1 && num3 > num2)
+				{
+					putchar (num1);
+					putchar (num2);
+					putchar (num3);
+					if (num1 != 55 ||  num2 != 56 || num3 != 57)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	return (0);
+}
+
+
