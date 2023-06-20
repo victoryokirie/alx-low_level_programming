5cee6656e45d1081bfd4da8df78cb95bfa77680a
victory okirie <victoryokirie98@gmail.com>
2023-06-20 11:34:09 -0700

    betty doc

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
-* _islower - checks if a charactec is lowercase and returns 1 if a character is lowercase
+* _islower - checks if a charactec is lowercase and returns 1 if yes
 * it returns 0 just like islower() in ctype.h
-* @c - this is the int value to be compared with ASCII value
+* @c: this is the int value to be compared with ASCII value
 * Return: 0 if successful
 */
 int _islower(int c)
