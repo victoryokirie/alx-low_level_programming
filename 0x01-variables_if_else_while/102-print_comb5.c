92159b4f528470fcaec752041bdf3bd57209cf02
victory okirie <victoryokirie98@gmail.com>
2023-06-19 19:28:17 -0700

    betty corrections

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,16 +10,16 @@ int main(void)
 
 	for (a = 0; a <= 99; a++)
 	{
-		for (b = 0; b <=99; b++)
+		for (b = 0; b <= 99; b++)
 		{
 			if (a < b)
 			{
 				putchar((a / 10) + 48);
 				putchar((a % 10) + 48);
 				putchar(' ');
-				putchar((b / 10) +48);
-				putchar((b % 10) +48);
-				if ( a != 98 || b != 99)
+				putchar((b / 10) + 48);
+				putchar((b % 10) + 48);
+				if (a != 98 || b != 99)
 				{
 					putchar(',');
 					putchar(' ');
