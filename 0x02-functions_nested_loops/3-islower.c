da5f576f92e4ee6fb36f86b3b25a8f9c693855d0
victory okirie <victoryokirie98@gmail.com>
2023-06-20 11:23:05 -0700

    betty corrections

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,8 @@
 #include "main.h"
 /**
-* _islower - to check if a character is lower case or not
+* _islower - to check if a character is lowercase and returns 1 if true
+* return 0 if character is not lowercase
+*@c - int value to be compared with ASCII value
 *
 * return: 0 (success)
 */
@@ -10,5 +12,4 @@ int _islower(int c)
 		return (1);
 	else
 		return (0);
-}
-	
+}	
