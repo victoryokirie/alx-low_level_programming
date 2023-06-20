b41e5786788c69fbb51a3a70c01cff6e12b70f54
victory okirie <victoryokirie98@gmail.com>
2023-06-20 11:55:53 -0700

    return value

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
-* _isalpha - returns 1 if c is a letter, lowercase or uppercase
+* _isalpha - Return 1 if c is a letter, lowercase or uppercase
 * @c: int value to be compared with ASCII value
-*
-* Return 1 if alpba else return 0
+* Return 1 if alpbabet else return 0
 */
 int _isalpha(int c)
 {
