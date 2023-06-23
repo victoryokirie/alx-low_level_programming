c2086413231e07861fb857ea611b1eb9547c74bb
victory okirie <victoryokirie98@gmail.com>
2023-06-23 01:36:34 -0700

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+*more_numbers - print 0 - 14 ten times
+*/
+void more_numbers(void)
+{
+	int a, b;
+
+	a = 0;
+
+	while (a < 10)
+	{
+		for (b=0; b <= 14; b++)
+		{
+			if (b >= 10)
+			{
+				_putchar((b / 10) + 48);
+			}
+
+			_putchar((b % 10) + 48);
+		}
+		_putchar('\n');
+
+		a++;
+	}
+}
