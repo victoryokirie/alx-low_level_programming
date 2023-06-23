ba3515144050cf986da4d3b8ff030caf97a1c2c0
victory okirie <victoryokirie98@gmail.com>
2023-06-22 18:32:42 -0700

    white spaces

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * _isdigit - Returns 1 if a character is digit, otherwise returns 0
- * @c: int value compared with ASCII value
- * Return: 0 if sucessful
- */
+* _isdigit - Returns 1 if a character is digit, otherwise returns 0
+* @c: int value compared with ASCII value
+* Return: 0 if sucessful
+*/
 int _isdigit (int c)
 {
 	if (c >= 48 && c < 58)
