79216af51c21b9f7b9380ee1312bf209bc6d0154
victory okirie <victoryokirie98@gmail.com>
2023-06-22 18:53:41 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*print_numbers - prints numbers 0 to 9
+*Return: 0 (success)
+*/
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+		_putchar(i);
+	_putchar('\n');
+}
