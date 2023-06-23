8af895106ad96300330994e8bc97b6ca879b638f
victory okirie <victoryokirie98@gmail.com>
2023-06-23 07:35:51 -0700

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*print_square - prints squares
+*@size: param
+*/
+void print_square(int size)
+{
+	int i, j;
+	
+	while (size > 0)
+	{
+		for (i =0; i < size; i++)
+		{
+		}
+	}
+}
