d86ce9ae45f671fe4cab821041b2af74e2b265c1
victory okirie <victoryokirie98@gmail.com>
2023-06-22 19:23:17 -0700

    print mn update

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,18 +1,15 @@
 #include "main.h"
 /**
 *print_most_numbers - prints numbers 0 to 9
-*Return: 0 (success)
 */
 void print_most_numbers(void)
 {
-        int i;
+	int i;
 
-        for (i = 48; i < 58; i++)
+	for (i = 48; i < 58; i++)
 	{
-		if (i == 50 || i == 52)
-			continue;
-		else
-                	_putchar(i);
+		if (i != 50 && i != 52)
+			_putchar(i);
 	}
-        _putchar('\n');
+	_putchar('\n');
 }
