c8d12c77a4b8b0d2c476963e087b067a9c7aefe0
victory okirie <victoryokirie98@gmail.com>
2023-06-26 02:24:26 -0700

    swap integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*swap_int - function that swaps the value of two integers
+*@a : first integer deference operator
+*@b : second integer deference operator
+*int temp: used to assign the value of a
+*Author: VO
+*Return: 0 (success)
+*/
+void swap_int(int *a, int *b)
+{
+	int temp;
+	
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
