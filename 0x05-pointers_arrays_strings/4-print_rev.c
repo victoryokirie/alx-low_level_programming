4640a4e40a4f3f41cbb546899f67964e41abaaef
victory okirie <victoryokirie98@gmail.com>
2023-06-26 03:35:58 -0700

    white spaces

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,9 +2,8 @@
 #include "main.h"
 #include <string.h>
 /**
-*print_rev - a function that prints a string in reverse order followed by a new line
+*print_rev - a function that prints a string in reverse order with new line
 *@s: set of string to be printed in reverse order
-*@length : string length
 *int i: string length decrementor
 **/
 void print_rev(char *s)
@@ -12,7 +11,7 @@ void print_rev(char *s)
 	int length = strlen(s);
 
 	int i = (length - 1);
-	
+
 	while (i >= 0)
 	{
 		putchar(s[i]);
