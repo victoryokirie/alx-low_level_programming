64f701c3afb91515db1ea98a0e7e44c4c2acbbf0
victory okirie <victoryokirie98@gmail.com>
2023-06-26 03:10:40 -0700

    return value

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -4,8 +4,10 @@
 *_puts - function that prints a string and a new line to stdout
 *@str: string to be printed to stdout
 *Author: VO
+*Return: 0 (success)
 **/
 int _puts(char *str)
 {
 	puts(str);
+	return (0);
 }
