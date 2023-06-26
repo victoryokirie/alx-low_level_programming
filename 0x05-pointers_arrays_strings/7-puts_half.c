038986664ec7af1538e79b270c629e40ef876c73
victory okirie <victoryokirie98@gmail.com>
2023-06-26 09:12:42 -0700

    length

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,12 +10,14 @@ void puts_half(char *str)
 	int length = strlen(str);
 	int i;
 
-	if (length % 2 != 0)
-		length = length - 1;
-	i = length / 2;
+	if (length % 2 == 1)
+		i = (length - 1) / 2;
+	else
+		i = length / 2;
 	while (i < length)
 	{
 		putchar(str[i]);
 		i++;
 	}
+	putchar('\n');
 }
