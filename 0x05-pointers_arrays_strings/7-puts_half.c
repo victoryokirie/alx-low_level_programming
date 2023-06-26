279100af3d23f7b5148c85a5e5173f9dbfe3e6a9
victory okirie <victoryokirie98@gmail.com>
2023-06-26 09:02:07 -0700

    betty

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -4,8 +4,6 @@
 /**
 * puts_half - prints half of string
 * @str : string to be split in two
-* @length : str length
-* @i : midpoint
 */
 void puts_half(char *str)
 {
@@ -13,12 +11,11 @@ void puts_half(char *str)
 	int i;
 
 	if (length % 2 != 0)
-		length = length + 1;
+		length = length - 1;
 	i = length / 2;
 	while (i < length)
 	{
 		putchar(str[i]);
 		i++;
 	}
-	putchar('\n');
 }
