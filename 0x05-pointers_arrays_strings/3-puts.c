5897f275c667caaa217e38cc9f97bd42b6863ac1
victory okirie <victoryokirie98@gmail.com>
2023-06-26 03:07:13 -0700

    stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*_puts - function that prints a string and a new line to stdout
+*@str: string to be printed to stdout
+*Author: VO
+*Return : 0
+**/
+int _puts(char *str)
+{
+	puts(str);
+	return (0);
+}
