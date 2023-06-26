3fbf38fbe37bb6fd81dbef546ec1345e48df1a9c
victory okirie <victoryokirie98@gmail.com>
2023-06-26 03:08:54 -0700

    stdout betty

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -4,10 +4,8 @@
 *_puts - function that prints a string and a new line to stdout
 *@str: string to be printed to stdout
 *Author: VO
-*Return : 0
 **/
 int _puts(char *str)
 {
 	puts(str);
-	return (0);
 }
