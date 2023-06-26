87299278a165ae305173e285e5ab4e135c4f0b8a
victory okirie <victoryokirie98@gmail.com>
2023-06-26 10:28:11 -0700

    array update

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,10 +9,10 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n -  1; i++)
+	for (i = 0; i < n; i++)
 	{
 		printf("%d", *(a + i));
-		if (i != (n - 1))
+		if (i != n)
 			printf(", ");
 	}
 	printf("\n");
