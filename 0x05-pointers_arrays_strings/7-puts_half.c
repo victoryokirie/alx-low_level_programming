142005c8ce18f664e34c1e6592e4ebaa7d8d4f5e
victory okirie <victoryokirie98@gmail.com>
2023-06-26 08:59:00 -0700

    print half update

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -2,23 +2,23 @@
 #include "main.h"
 #include <string.h>
 /**
-* puts_half - function tht pritns second half of a string
+* puts_half - prints half of string
 * @str : string to be split in two
 * @length : str length
-* @start : midpoint
+* @i : midpoint
 */
 void puts_half(char *str)
 {
 	int length = strlen(str);
-	int start;
+	int i;
 
 	if (length % 2 != 0)
-		length = length - 1;
-	start = length / 2;
-	while (start < length)
+		length = length + 1;
+	i = length / 2;
+	while (i < length)
 	{
-		putchar(str[start]);
-		start++;
+		putchar(str[i]);
+		i++;
 	}
 	putchar('\n');
 }
