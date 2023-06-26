4255db6bdb9511864902965d91591245a1c28d1f
victory okirie <victoryokirie98@gmail.com>
2023-06-26 02:12:14 -0700

    reset

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 /**
-*rest_to_98 - function to reset the value of variable to 98
+*reset_to_98 - function to reset the value of variable to 98
 *@n: pointer deference
 *Author: VO
 *Return: 0 (Success)
