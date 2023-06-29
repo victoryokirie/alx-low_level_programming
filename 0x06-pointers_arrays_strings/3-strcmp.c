75ce538136f7931a489dea6d8c7d1070b285e128
victory okirie <victoryokirie98@gmail.com>
2023-06-29 12:24:28 -0700

    return value

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,8 +3,7 @@
 *_strcmp - function compares two strings
 *@s1: pointer to first string
 *@s2: pointer to second string
-*return : value greater than 0 if s1 > s2
-*value less than 0 if s2 > s1 or 0 if s1=s2
+*return : 0
 */
 int _strcmp(char *s1, char *s2)
 {
