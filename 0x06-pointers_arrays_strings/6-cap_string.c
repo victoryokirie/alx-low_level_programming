bd83b26ef091a845864cdd786448639a5fd021ed
victory okirie <victoryokirie98@gmail.com>
2023-07-01 05:44:31 -0700

    betty

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -17,13 +17,15 @@ char *cap_string(char *str)
 		if (str[0] >= 97 && str [0] <= 122)
 			str[i] = str[i] - 32;
 		if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
-		str[i] == ',' || str[i] == ';' || str[i] == '.' ||
-		str[i] == '?' || str[i] == '"' || str[i] == '(' ||
-		str[i] == ')' || str[i] == '{' || str[i] == '}')
+		    str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+		    str[i] == '?' || str[i] == '"' || str[i] == '(' ||
+		    str[i] == ')' || str[i] == '{' || str[i] == '}')
 		{
 			if (str[i + 1] >= 97 && str[i +1] <= 122)
 				str[i + 1] = str[i + 1] - 32;
-		}i++;
+		}
+		i++;
 	}
+
 	return (str);
 }
